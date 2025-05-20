fa7686a35ffd058f5ef72a9f061d48084e6dcf52
Alessandro Ferrini <alessandro.ferrini@apptecsrl.com>
2025-05-20 11:37:55 +0200

    create and get short

--- src/url_shortener/views.py
+++ src/url_shortener/views.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+urls_mapping = {}
+
 
 @app.get("/")
 def read_root():
@@ -15,7 +17,28 @@ def health_check():
     return {"status": "ok", "database": "ok", "cache": "ok"}
 
 
-#
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.post("/short-urls")
+def create_short(long_url: str, alias: str):
+    urls_mapping["alias"] = long_url
+
+    return {
+        "long_url": f"{long_url}",
+        "short_url": f"localhost:8000/{alias}",
+        "alias": alias,
+    }
+
+
+@app.get("/short-urls/{alias}")
+def get_short(alias: str):
+    long_url = urls_mapping.get("alias", None)
+
+    return {
+        "long_url": f"{long_url}",
+        "short_url": f"localhost:8000/{alias}",
+        "alias": alias,
+    }
